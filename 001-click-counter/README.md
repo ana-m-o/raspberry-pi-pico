dab493b42566575593c3229805812a5cb8d5719a
Ana Marín <anamarinorgaz@gmail.com>
2022-09-21 16:41:33 +0200

    Add LCD screen to show counter

--- 001-click-counter/README.md
+++ 001-click-counter/README.md
@@ -4,7 +4,16 @@
 
 - Raspberry Pi Pico (+ usb cable, breadboard and jumper wires).
 - 2 push buttons
+- I2C LCD1602 RGB Display.
 
 ## Function
 
-The click counter shows on the terminal the number of clicks made with a button. There is a second button for reset counter to 0.
+The click counter shows on the screen the number of clicks made with a button. There is a second button for reset counter to 0.
+
+## Connections
+
+Wire diagram:
+
+![](./images/protoboard-1.png)
+
+Left button is button #1 and right button is #2.
--- 001-click-counter/main.py
+++ 001-click-counter/main.py
@@ -1,18 +1,43 @@
-import machine
+import RGB1602
 import utime
+from machine import Pin
 
-button_1 = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_DOWN)
-button_2 = machine.Pin(8, machine.Pin.IN, machine.Pin.PULL_DOWN)
+button_1 = Pin(8, Pin.IN, Pin.PULL_DOWN)
+button_2 = Pin(10, Pin.IN, Pin.PULL_DOWN)
+
+lcd = RGB1602.RGB1602(16, 2)
+rgb = (0, 128, 60)
+dangerRgb = (255, 0, 0)
+
 count = 0
+
+# Default screen color
+lcd.setRGB(rgb[0], rgb[1], rgb[2])
+lcd.setCursor(0, 0)
+lcd.printout(count)
 
 while True:
 
+    # Add Button
     if button_1.value() == 1:
         count = count + 1
-        utime.sleep(0.5)
-        print("Counter: ", count)
+        utime.sleep(0.3)
+        lcd.clear()
+        lcd.printout(count)
 
+    # Reset button
     if button_2.value() == 1:
         count = 0
-        utime.sleep(0.5)
-        print("--Reset--\nCounter: ", count)
+        lcd.clear()
+
+        # Momentary red screen color for visual feedback
+        lcd.setRGB(dangerRgb[0], dangerRgb[1], dangerRgb[2])
+        lcd.printout("Reset")
+
+        # Wait 1 sec and clear screen
+        utime.sleep(1)
+        lcd.clear()
+
+        # Default screen color
+        lcd.setRGB(rgb[0], rgb[1], rgb[2])
+        lcd.printout(count)
